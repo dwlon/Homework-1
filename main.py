8370987d49273445182ce04a2dcc7810c576878b
DrilonM <blastplaysmc@gmail.com>
2024-10-30 21:33:01 +0100

    Version that looks through all companies, ETA 26 minutes

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import date, timedelta
 from selenium import webdriver
+from selenium.common import TimeoutException
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+
+st = time.time()
 
 chrome_options = Options()
 
 # Uncomment these if you do not want chrome to pop-up
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.implicitly_wait(10)
+# this is only waiting for clarity when looking at the chrome page, can be removed for faster performance
+# driver.implicitly_wait(10)
+
 def has_digits(string):
     return any(char.isdigit() for char in string)
 
@@ -32,12 +38,12 @@ soup = BeautifulSoup(raw_html, "html.parser")
 codes = soup.select_one("#Code").select("option")
 
 # parsedCodes currently has 1 code for testing
-parsedCodes = ["ADIN"]
+parsedCodes = []
 
 # parsing through all the company codes and adding the to the array
-# for code in codes:
-#     if not code.text.startswith("E")  and not has_digits(code.text):
-#         parsedCodes.append(code.text)
+for code in codes:
+    if not code.text.startswith("E")  and not has_digits(code.text):
+        parsedCodes.append(code.text)
 
 
 #  For each Company code
@@ -48,10 +54,9 @@ for code in parsedCodes:
 
     driver.get(url)
 
-    current = date.today()
+    print(code)
 
-    # toDate = soup.select_one("#ToDate")
-    # fromDate = soup.select_one("#FromDate")
+    current = date.today()
 
 # Grabbing data for the last 10 years
     for i in range(0, 10):
@@ -73,9 +78,20 @@ for code in parsedCodes:
         buttons = driver.find_elements(By.CLASS_NAME, "btn")
         buttons[1].click()
 
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "table"))
-        )
+        try:
+            WebDriverWait(driver, 3).until(
+                EC.presence_of_element_located((By.TAG_NAME, "table"))
+            )
+        except TimeoutException:
+            break
 
         table = pd.read_html(driver.page_source)
         print(table)
+
+driver.quit()
+
+et = time.time()
+
+total_time = et - st
+
+print("Execution time: ", total_time, "seconds")

cb7fc6f472325651bab4a38ee4208c6846f98c53
DrilonM <blastplaysmc@gmail.com>
2024-10-31 18:01:58 +0100

    Fixed the StringIO error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import concurrent.futures
+from io import StringIO
+
 from bs4 import BeautifulSoup
 import requests
 from datetime import date, timedelta
@@ -12,12 +14,20 @@ import pandas as pd
 import time
 import os
 
-num_threads = os.cpu_count() / 2
+num_threads = int(os.cpu_count() / 2)
 print(num_threads)
+
+dates = {}
+
+currentTime = date.today()
+
+for i in range(0, 10):
+    yearAgo = currentTime - timedelta(days=365)
+    dates[currentTime] = yearAgo
+    currentTime = currentTime - timedelta(days=365)
 
 def scrape(url):
     chrome_options = Options()
-
     # Comment these if you want chrome to pop-up
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
@@ -27,23 +37,16 @@ def scrape(url):
 
     driver.get(url)
 
-    current = date.today()
-
-    for i in range(0, 10):
-        year_ago = current - timedelta(days=365)
-
+    for to_date, from_date in dates.items():
         toDate = driver.find_element(By.ID, "ToDate")
         fromDate = driver.find_element(By.ID, "FromDate")
 
         fromDate.clear()
         toDate.clear()
-
-        fromDate.send_keys(year_ago.strftime("%m/%d/%Y"))
-        toDate.send_keys(current.strftime("%m/%d/%Y"))
+        fromDate.send_keys(from_date.strftime("%m/%d/%Y"))
+        toDate.send_keys(to_date.strftime("%m/%d/%Y"))
 
         print("Data from: " + fromDate.get_attribute("value") + " - to: " + toDate.get_attribute("value"))
-
-        current -= timedelta(days=365)
 
         buttons = driver.find_elements(By.CLASS_NAME, "btn")
         buttons[1].click()
@@ -55,10 +58,10 @@ def scrape(url):
         except TimeoutException:
             break
 
-        table = pd.read_html(driver.page_source)
+        table = pd.read_html(StringIO(driver.page_source))
         print(table)
 
-    driver.close()
+    driver.quit()
     return
 
 
@@ -81,7 +84,7 @@ for code in codes:
         parsedCodes.append(code.text)
 
 #currently at 5 max, will stress test later
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers = num_threads) as executor:
     futures = [
         executor.submit(scrape, f"https://www.mse.mk/en/stats/symbolhistory/{code}")
         for code in parsedCodes

13f084ea66df5b829298be8edbc30f5ae716c73c
DrilonM <blastplaysmc@gmail.com>
2024-10-31 12:52:41 +0100

    Version with 5 threads, takes 7.7 minutes

--- main.py
+++ main.py
@@ -1,56 +1,26 @@
+import threading
+import concurrent.futures
 from bs4 import BeautifulSoup
 import requests
 from datetime import date, timedelta
 from selenium import webdriver
 from selenium.common import TimeoutException
 from selenium.webdriver.chrome.options import Options
-import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 import time
 
-st = time.time()
+def scrape(url):
+    chrome_options = Options()
 
-chrome_options = Options()
+    # Comment these if you want chrome to pop-up
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
 
-# Comment these if you want chrome to pop-up
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-driver = webdriver.Chrome(options=chrome_options)
-
-# this is only waiting for clarity when looking at the chrome page, can be removed for faster performance
-# driver.implicitly_wait(10)
-
-def has_digits(string):
-    return any(char.isdigit() for char in string)
-
-url="https://www.mse.mk/en/stats/symbolhistory/ALK"
-
-response = requests.get(url)
-
-raw_html = response.text
-
-soup = BeautifulSoup(raw_html, "html.parser")
-
-codes = soup.select_one("#Code").select("option")
-
-
-parsedCodes = []
-
-# parsing through all the company codes and adding the to the array
-for code in codes:
-    if not code.text.startswith("E")  and not has_digits(code.text):
-        parsedCodes.append(code.text)
-
-
-#  For each Company code
-for code in parsedCodes:
-    url = "https://www.mse.mk/en/stats/symbolhistory/" + code
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
+    driver = webdriver.Chrome(options=chrome_options)
 
     driver.get(url)
 
@@ -58,9 +28,8 @@ for code in parsedCodes:
 
     current = date.today()
 
-# Grabbing data for the last 10 years
     for i in range(0, 10):
-        year_ago = current - timedelta(days=364)
+        year_ago = current - timedelta(days=365)
 
         toDate = driver.find_element(By.ID, "ToDate")
         fromDate = driver.find_element(By.ID, "FromDate")
@@ -71,15 +40,15 @@ for code in parsedCodes:
         fromDate.send_keys(year_ago.strftime("%m/%d/%Y"))
         toDate.send_keys(current.strftime("%m/%d/%Y"))
 
-        print("from: " + fromDate.get_attribute("value") + " - to: " + toDate.get_attribute("value"))
+        print("Data from: " + fromDate.get_attribute("value") + " - to: " + toDate.get_attribute("value"))
 
-        current -= timedelta(days=364)
+        current -= timedelta(days=365)
 
         buttons = driver.find_elements(By.CLASS_NAME, "btn")
         buttons[1].click()
 
         try:
-            WebDriverWait(driver, 3).until(
+            WebDriverWait(driver, 1).until(
                 EC.presence_of_element_located((By.TAG_NAME, "table"))
             )
         except TimeoutException:
@@ -87,11 +56,34 @@ for code in parsedCodes:
 
         table = pd.read_html(driver.page_source)
         print(table)
+    return
 
-driver.quit()
+
+def has_digits(string):
+    return any(char.isdigit() for char in string)
+
+st = time.time()
+
+response = requests.get("https://www.mse.mk/en/stats/symbolhistory/ALK")
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+codes = soup.select_one("#Code").select("option")
+
+parsedCodes = []
+
+# parsing through all the company codes and adding the to the array
+for code in codes:
+    if not code.text.startswith("E")  and not has_digits(code.text):
+        parsedCodes.append(code.text)
+
+#currently at 5 max, will stress test later
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    futures = [
+        executor.submit(scrape, f"https://www.mse.mk/en/stats/symbolhistory/{code}")
+        for code in parsedCodes
+    ]
 
 et = time.time()
-
 total_time = et - st
-
 print("Execution time: ", total_time, "seconds")

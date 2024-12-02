d15f9090eb108b68d52825542131c63f4c6621a7
DrilonM <blastplaysmc@gmail.com>
2024-12-02 20:17:23 +0100

    added Tech Prototype

---
+++ Homework2/tech_prototype/spring/src/main/java/com/example/demo/config/DatasourceConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:stock_data.db");
+        return dataSource;
+    }
+
+}
---
+++ Homework2/tech_prototype/spring/src/main/java/com/example/demo/service/IndexService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+
+import com.example.demo.entities.Index;
+
+import java.util.List;
+
+public interface IndexService {
+    public List<Index> findAll();
+}

9420f16f71fbb5149a944ac6d145a3b27a3227d3
Endrit Hasani <endrithasani2019@gmail.com>
2024-12-12 18:34:07 +0100

    Test commit 2

--- Homework3/stockMarketApp/src/main/java/com/stockmarketapp/repository/IndexRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stockmarketapp.repository;
-
-import com.stockmarketapp.entities.Index;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IndexRepository extends JpaRepository<Index, Long> {
-}
-
